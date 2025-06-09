e5fbea8b76abe7ff1dcf0dff607bcfee1014f0cc
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-09 16:22:07 +0900

    [Update]TopController
    ・topメソッド

--- src/main/java/com/example/patas_board/controller/TopController.java
+++ src/main/java/com/example/patas_board/controller/TopController.java
@@ -1,7 +1,27 @@
 package com.example.patas_board.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class TopController {
+
+    /*
+     * 投稿内容表示処理
+     */
+    @GetMapping
+    public ModelAndView top() {
+        ModelAndView mav = new ModelAndView();
+
+        // 画面遷移先を指定
+        mav.setViewName("/top");
+        return mav;
+    }
+
 }
