457e3c8ab37c50ce63ab54851cf830a8d6fcddee
tanaka_rua <tanaka.rua@alhinc.jp>
2025-06-09 13:15:25 +0900

    firstcommit

---
+++ src/main/java/com/example/patas_board/PatasBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.patas_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatasBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatasBoardApplication.class, args);
+	}
+
+}
