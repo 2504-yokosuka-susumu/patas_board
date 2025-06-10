8492390e5dd09eb632da3f14dbf169eeebaf2de7
tanaka_rua <tanaka.rua@alhinc.jp>
2025-06-10 11:00:19 +0900

    【Add】新規投稿画面の実装

--- src/main/java/com/example/patas_board/controller/LoginController.java
+++ src/main/java/com/example/patas_board/controller/LoginController.java
@@ -2,6 +2,11 @@ package com.example.patas_board.controller;
 
 import com.example.patas_board.controller.form.UserForm;
 import com.example.patas_board.repository.entity.User;
+import com.example.patas_board.service.UserService;
+import com.example.patas_board.utils.CipherUtil;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -11,11 +16,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.crypto.Cipher;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private HttpSession session;
+
+    @Autowired
+    private Cipher encrypter;
+
     @GetMapping("/login/form")
     public ModelAndView view(){
             ModelAndView mav = new ModelAndView();
@@ -28,19 +44,32 @@ public class LoginController {
             return mav;
     }
 
-//    @PostMapping("/login")
-//    public ModelAndView login(@ModelAttribute("userForm") @Validated UserForm userForm, BindingResult result){
-//
-//        if(result.hasErrors()){
-//            ModelAndView mav = new ModelAndView();
-//            List<String> errorMessages = new ArrayList<String>();
-//            for(ObjectError error:result.getAllErrors()){
-//                errorMessages.add(error.getDefaultMessage());
-//            }
-//            mav.setViewName("/login/form");
-//            return mav;
-//        }else{
-//            User user = userService.login();
-//        }
-//    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("userForm") @Validated UserForm userForm, BindingResult result, HttpRequest request){
+
+        if(result.hasErrors()){
+            ModelAndView mav = new ModelAndView();
+            List<String> errorMessages = new ArrayList<String>();
+            for(ObjectError error:result.getAllErrors()){
+                errorMessages.add(error.getDefaultMessage());
+            }
+            mav.setViewName("/login/form");
+            return mav;
+        }else{
+            ModelAndView mav = new ModelAndView();
+            String account = userForm.getAccount();
+            String password = userForm.getPassword();
+            String encPassword = CipherUtil.encrypt(password);
+            UserForm user = userService.login(account, encPassword);
+            if(user == null || user.getIsStopped() == 1){
+                String error = "ログインに失敗しました";
+                mav.addObject("error", error);
+                mav.setViewName("/login/form");
+                return mav;
+            }
+            session.setAttribute("loginUser", user);
+            mav.setViewName("redirect:/");
+            return mav;
+        }
+    }
 }
--- src/main/java/com/example/patas_board/service/UserService.java
+++ src/main/java/com/example/patas_board/service/UserService.java
@@ -1,7 +1,47 @@
 package com.example.patas_board.service;
 
+import com.example.patas_board.controller.form.UserForm;
+import com.example.patas_board.repository.UserRepository;
+import com.example.patas_board.repository.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public UserForm login(String account, String password){
+        List<User> results = new ArrayList<>();
+        results.add((User)userRepository.findByAccountAndPassword(account, password));
+
+        if(results.contains(null)) {
+            return null;
+        }else{
+            List<UserForm> tasks = setUserForm(results);
+            return tasks.get(0);
+        }
+    }
+
+    private List<UserForm> setUserForm(List<User> results){
+        List<UserForm> users = new ArrayList<>();
+
+        for (int i = 0; i < results.size(); i++) {
+            UserForm user = new UserForm();
+            User result = results.get(i);
+            user.setId(result.getId());
+            user.setAccount(result.getAccount());
+            user.setPassword(result.getPassword());
+            user.setName(result.getName());
+            user.setBranchId(result.getBranchId());
+            user.setDepartmentId(result.getDepartmentId());
+            user.setIsStopped(result.getIsStopped());
+            users.add(user);
+        }
+        return users;
+    }
 }
