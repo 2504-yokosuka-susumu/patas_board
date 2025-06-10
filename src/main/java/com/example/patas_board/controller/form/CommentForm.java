4f4a1d7be6c70a4da7cb7eb2707ed1093b70c003
nakano_keisuke <nakano.keisuke@alhinc.jp>
2025-06-10 11:53:37 +0900

    [Update] コメントのバリデーション

--- src/main/java/com/example/patas_board/controller/form/CommentForm.java
+++ src/main/java/com/example/patas_board/controller/form/CommentForm.java
@@ -1,6 +1,8 @@
 package com.example.patas_board.controller.form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.Date;
 
@@ -9,6 +11,8 @@ public class CommentForm {
 
     private int id;
 
+    @NotBlank(message = "メッセージを入力してください")
+    @Length(message = "500文字以内で入力してください")
     private String text;
 
     private int userId;
