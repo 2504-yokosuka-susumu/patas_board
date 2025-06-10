bad09b3850d776e0a49c2bf70be515e100975906
tanaka_rua <tanaka.rua@alhinc.jp>
2025-06-10 09:31:56 +0900

    【Add】ログイン画面の実装

--- src/main/java/com/example/patas_board/controller/LoginController.java
+++ src/main/java/com/example/patas_board/controller/LoginController.java
@@ -1,10 +1,18 @@
 package com.example.patas_board.controller;
 
 import com.example.patas_board.controller.form.UserForm;
+import com.example.patas_board.repository.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -19,4 +27,20 @@ public class LoginController {
             mav.addObject("userForm", userForm);
             return mav;
     }
+
+//    @PostMapping("/login")
+//    public ModelAndView login(@ModelAttribute("userForm") @Validated UserForm userForm, BindingResult result){
+//
+//        if(result.hasErrors()){
+//            ModelAndView mav = new ModelAndView();
+//            List<String> errorMessages = new ArrayList<String>();
+//            for(ObjectError error:result.getAllErrors()){
+//                errorMessages.add(error.getDefaultMessage());
+//            }
+//            mav.setViewName("/login/form");
+//            return mav;
+//        }else{
+//            User user = userService.login();
+//        }
+//    }
 }
