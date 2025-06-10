e4bb222c0c2b3d7f972307908963f352f5f5515f
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-10 16:49:21 +0900

    [Add]setting.html
    ・ユーザ編集画面
    ・まだPostで登録はできません
    ・BranchとDepartmentの名前取得まだです
    ・バリデーションまだです

--- src/main/java/com/example/patas_board/controller/UserEditController.java
+++ src/main/java/com/example/patas_board/controller/UserEditController.java
@@ -1,11 +1,11 @@
 package com.example.patas_board.controller;
 
+import com.example.patas_board.controller.form.BranchForm;
 import com.example.patas_board.controller.form.UserForm;
 import com.example.patas_board.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,18 +15,33 @@ public class UserEditController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/setting/form")
-    public ModelAndView view(@PathVariable("id") int id) {
+    @GetMapping("/setting/form/{id}")
+    public ModelAndView view(@PathVariable String id) {
         // 管理者権限フィルター
 
         // ユーザ情報取得
-        UserForm userForm = new UserForm();
-        List<UserForm> userData = userService.findAllUser();
+        int userId = Integer.parseInt(id);
+        UserForm userData = userService.selectUser(userId);
 
         // "users"オブジェクト "statuses"オブジェクト　格納
         ModelAndView mav = new ModelAndView();
         mav.addObject("users",userData);
-        mav.setViewName("/edit");
+        mav.setViewName("/setting");
         return mav;
+    }
+
+    /*
+     * ユーザ編集更新処理
+     */
+    @PostMapping("/setting")
+    public ModelAndView updateUser(@ModelAttribute("formModel") UserForm userForm) {
+
+        // User情報をFormに格納
+
+        // ステータス更新処理
+        userService.updateUser(userForm);
+
+        // rootへリダイレクト
+        return new ModelAndView("redirect:/");
     }
 }
