7e73efd5a31c05fa6082e9346dd287e0602f2551
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-10 09:18:22 +0900

    [Add]logOut
    ・logOut
    ・ユーザ管理画面　下地

--- src/main/java/com/example/patas_board/controller/TopController.java
+++ src/main/java/com/example/patas_board/controller/TopController.java
@@ -4,13 +4,12 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class TopController {
+    @Autowired
+    HttpSession session;
 
     /*
      * 投稿内容表示処理
@@ -24,4 +23,17 @@ public class TopController {
         return mav;
     }
 
+    /*
+     * ログアウト処理
+     */
+    @GetMapping("/logout")
+    public ModelAndView logOut() {
+        // session情報取得
+        session = (HttpSession) session.getAttribute("loginUser");
+
+        // セッションの無効化
+        session.invalidate();
+        // rootへリダイレクト
+        return new ModelAndView("redirect:/");
+    }
 }
--- src/main/java/com/example/patas_board/controller/UserMessageController.java
+++ src/main/java/com/example/patas_board/controller/UserMessageController.java
@@ -1,7 +1,48 @@
 package com.example.patas_board.controller;
 
+import com.example.patas_board.service.MessageService;
+import com.example.patas_board.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserMessageController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    HttpSession session;
+
+    @GetMapping("/manager/form")
+    public ModelAndView view() {
+        // 管理者権限フィルター
+
+        // session情報取得
+        session = (HttpSession) session.getAttribute("loginUser");
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/manager");
+        return mav;
+    }
+
+    /*
+     * アカウント復活/停止切り替え
+     */
+    @PostMapping("/manager")
+    public ModelAndView changeStatus(){
+        ModelAndView mav = new ModelAndView();
+
+        return mav;
+    }
 }
