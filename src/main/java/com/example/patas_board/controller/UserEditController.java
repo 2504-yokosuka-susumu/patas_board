b68fcb4ee40128f6c4eaea499efd62df361d4f3f
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-10 13:55:13 +0900

    [Add]setting
    ・setting.html 追加
    ・UserEditController 変更

--- src/main/java/com/example/patas_board/controller/UserEditController.java
+++ src/main/java/com/example/patas_board/controller/UserEditController.java
@@ -1,7 +1,32 @@
 package com.example.patas_board.controller;
 
+import com.example.patas_board.controller.form.UserForm;
+import com.example.patas_board.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class UserEditController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/setting/form")
+    public ModelAndView view(@PathVariable("id") int id) {
+        // 管理者権限フィルター
+
+        // ユーザ情報取得
+        UserForm userForm = new UserForm();
+        List<UserForm> userData = userService.findAllUser();
+
+        // "users"オブジェクト "statuses"オブジェクト　格納
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("users",userData);
+        mav.setViewName("/edit");
+        return mav;
+    }
 }
