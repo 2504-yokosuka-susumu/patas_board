bdab3bde936d059c6b62c1d06cfc4611f4f86643
nakano_keisuke <nakano.keisuke@alhinc.jp>
2025-06-12 18:03:01 +0900

    [Bug fixes] 支社と部署のプルダウン　追加

---
+++ src/main/java/com/example/patas_board/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.example.patas_board.repository;
+
+import com.example.patas_board.repository.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+}
