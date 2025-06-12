04afd2139eb41533ce32b8e0d2e925c70f207959
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-12 11:26:27 +0900

    [Update]ユーザ編集バリデーション機能

--- src/main/java/com/example/patas_board/controller/UserEditController.java
+++ src/main/java/com/example/patas_board/controller/UserEditController.java
@@ -7,22 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class UserEditController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/setting/form/{id}")
-    public ModelAndView view(@PathVariable String id) {
-        // 管理者権限フィルター
-
+    @GetMapping("/setting/form")
+    public ModelAndView view(@RequestParam("id") String id) {
         // ユーザ情報取得
         int userId = Integer.parseInt(id);
         UserForm userData = userService.selectUser(userId);
@@ -39,32 +40,46 @@ public class UserEditController {
      */
     @PostMapping("/setting")
     public ModelAndView updateUser(@ModelAttribute("formModel") @Validated UserForm userForm, BindingResult result,
-                                   @RequestParam(name = "confirmPassword", required = false) String confirmPassword,
-                                   RedirectAttributes redirectAttributes) {
+                                   @RequestParam(name = "confirmPassword", required = false) String confirmPassword) {
 
         ModelAndView mav = new ModelAndView();
+        List<String> errorMessages = new ArrayList<String>();
 
         // Formバリデーションチェック
         if (result.hasErrors()) {
-            List<String> errorMessages = new ArrayList<String>();
             for (ObjectError error : result.getAllErrors()) {
                 errorMessages.add(error.getDefaultMessage());
             }
             mav.setViewName("/setting");
-            return mav;
-        }
-        // パスワードが一致しているか
-        if(userForm.getPassword().equals(confirmPassword)) {
+        } else if (!Objects.equals(userForm.getPassword(), confirmPassword)) {
+            errorMessages.add("パスワードと確認用パスワードが一致しません");
+            mav.setViewName("/setting");
+        } else if ((userForm.getBranchId() == 1 || userForm.getBranchId() == 2) && userForm.getDepartmentId() != 1){
+            errorMessages.add("支社と部署の組み合わせが不正です");
+            mav.setViewName("/setting");
+        } else if ((userForm.getBranchId() == 3 || userForm.getBranchId() == 4) && userForm.getDepartmentId() == 1) {
+            errorMessages.add("支社と部署の組み合わせが不正です");
+            mav.setViewName("/setting");
+        } else {
+
+            String account = userForm.getAccount();
+            UserForm existAccount = userService.checkedAccount(account);
+
+            // アカウントが重複しているか
+            if (existAccount.getId() != userForm.getId()) {
+                errorMessages.add("アカウントが重複しています");
+                String settingUrl = "/setting/form?id=" + String.valueOf(userForm.getId());
+                mav.setViewName("/setting/form");
+                mav.addObject("errorMessages", errorMessages);
+                return mav;
+            }
+
             // ステータス更新処理
             userService.updateUser(userForm);
             mav.setViewName("redirect:/");
-        } else {
-            // エラーメッセージ格納
-            String error = "パスワードと確認用パスワードが一致しません";
-            redirectAttributes.addFlashAttribute("errorMessage", error);
-            mav.setViewName("/setting");
         }
 
+        mav.addObject("errorMessages", errorMessages);
         return mav;
     }
 }
